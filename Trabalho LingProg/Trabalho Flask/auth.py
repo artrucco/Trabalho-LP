31505681979023b155757e2af60953a6303a1745
artrucco <116761431+artrucco@users.noreply.github.com>
2022-11-03 04:43:25 -0300

    Branch 2 upar arquivo (#7)
    * Branch 1 (#1)
    
    * adicionando txt no ignore
    
    * commit 3 - criação de contas de usuario
    
    * commit 4 - login necessario para entrar na home
    
    * commit 5 - login e criação de usuario funcionando / anotações
    
    * commit 6 - upload de arquivo csv
    
    * commit 6 - upload de arquivo csv e leitura
    
    * commit 8 - quase tudo funcionando
    
    * commit 9 - fim
    
    * Create README.md (#6)

--- Trabalho LingProg/Trabalho Flask/auth.py
+++ Trabalho LingProg/Trabalho Flask/auth.py
@@ -3,7 +3,11 @@ from unicodedata import category
 from xmlrpc.client import boolean
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from models import User, db
+
+#para nunca guardar a senha como texto, converter para hash
+
 #para nunca guardar a senha como texto, converter a hash
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 
@@ -41,17 +45,36 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        primeiro_nome = request.form.get('primeiro_nome')
+        primeiro_nome = request.form.get('primeiroNome')
         senha1 = request.form.get('senha1')
         senha2 = request.form.get('senha2')
 
+
+        #percorre db pra checar se o usuario ja existe, registra o primeiro com o nome
+        user = User.query.filter_by(email=email).first()
+        #se o usuario existe...
+
         #checa se usuario ja existe
         user = User.query.filter_by(email=email).first()
+
         if user:
             flash('Email ja cadastrado.', category = 'error')
             return redirect(url_for('views.to_sign_up'))
         #checar validade do usuario novo
         if len(email) < 4:
+
+            flash('Email inválido', category='error')
+            return redirect(url_for('views.to_sign_up'))
+        elif len(primeiro_nome) < 2:
+            flash('Nome deve conter mais que dois caracteres', category='error')
+            return redirect(url_for('views.to_sign_up'))
+        elif senha1 != senha2:
+            flash('Senhas nao sao iguais', category='error')
+            return redirect(url_for('views.to_sign_up'))
+        elif len(senha1) < 7:
+            flash('Senha deve conter ao menos 7 caracteres', category='error')
+            return redirect(url_for('views.to_sign_up'))
+
             flash('Email deve ser maior', category='error')
             return redirect(url_for('auth.sign_up'))
         elif len(primeiro_nome) < 2:
@@ -63,14 +86,15 @@ def sign_up():
         elif len(senha1) < 7:
             flash('Senha deve conter ao menos 7 caracteres', category='error')
             return redirect(url_for('auth.sign_up'))
+
         else:
             #adiciona usuario novo a databse
             new_user = User(email=email, primeiro_nome=primeiro_nome, senha=generate_password_hash(senha1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember = True) 
+            #login_user(user, remember = True) 
             flash('Conta criada com sucesso!', category='success')
             #redireciona a funcao home em views - pagina inicial
-            return redirect(url_for('views.home'))
+            return render_template('login.html', user = new_user)
 
     return render_template("sign_up.html", user = current_user)
